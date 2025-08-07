f54a28195f000dede522a5a988d8905c2ea0a5db
lirunziii <lirunziii@gmail.com>
2025-08-07 15:01:34 +1000

    port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); // 用于解析 JSON 请求体
 app.use('/',Routes);
 
-app.listen(process.env.MYSQLPORT || 3306, () => {
+app.listen(process.env.PORT, () => {
   console.log('服务器启动');
 });
 
